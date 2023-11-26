56d909fb906ac58f10ac5a5973b12e03e997c256
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2023-11-26 16:10:49 -0500

    ScrollView added to the authvewcontroller

---
+++ Cruise Booking App/AuthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthViewController.swift
+//  Cruise Booking App
+//
+//  Created by Jawwad Abbasi on 2023-11-26.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
