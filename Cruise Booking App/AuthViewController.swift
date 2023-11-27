dd0f1693fad7c942b288a4ee5746c8799e97713e
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2023-11-26 21:20:30 -0500

    Added UI for loogin and signup

--- Cruise Booking App/AuthViewController.swift
+++ Cruise Booking App/AuthViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AuthViewController: UIViewController {
+class AuthViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailTextField: CustomTextField!
     
@@ -20,13 +20,61 @@ class AuthViewController: UIViewController {
         super.viewDidLoad()
         setBackground()
 
-        // Do any additional setup after loading the view.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+
+
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("TextField did begin editing")
+        // Additional code if needed
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+
     
     @IBAction func OnLoginButton(_ sender: Any) {
+        // Check entered credentials
+        let enteredEmail = emailTextField.text ?? ""
+        let enteredPassword = passwordTextField.text ?? ""
+
+        if enteredEmail == "test@gmail.com" && enteredPassword == "test123" {
+            // Credentials are correct, show login successful popup
+            showLoginSuccessfulPopup()
+        } else {
+            // Credentials are incorrect, show an error message or handle it as needed
+            print("Incorrect credentials")
+            showIncorrectCredentialsAlert()
+        }
     }
-    
+
+    func showLoginSuccessfulPopup() {
+        let alertController = UIAlertController(title: "Login Successful", message: "Welcome!", preferredStyle: .alert)
+
+        // Add an action to dismiss the alert
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            // Handle any additional actions after login success
+        }
+        alertController.addAction(okAction)
+
+        // Present the alert
+        present(alertController, animated: true, completion: nil)
+    }
+
+    func showIncorrectCredentialsAlert() {
+        let alertController = UIAlertController(title: "Incorrect Credentials", message: "Please check your email and password.", preferredStyle: .alert)
+
+        // Add an action to dismiss the alert
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+
+        // Present the alert
+        present(alertController, animated: true, completion: nil)
+    }
 
     func setBackground() {
         view.addSubview(backgroundImageView)
