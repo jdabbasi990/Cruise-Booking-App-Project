0ea0f20152ebd20a01267428c55601839e5022b8
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2023-12-09 14:32:50 -0500

    Added Detals screen for better view

---
+++ Cruise Booking App/Table View/CruiseViewDetails.swift
@@ -0,0 +1,26 @@
+//
+//  CruiseViewDetails.swift
+//  Cruise Booking App
+//
+//  Created by Jawwad Abbasi on 2023-12-09.
+//
+
+import UIKit
+
+class CruiseViewDetails: UIViewController {
+    
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    var selectedCruise : Cruise!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
